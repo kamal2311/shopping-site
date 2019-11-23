5d0fa337cef543fc39674793e6f784d269572458
Kamal Joshi <kamal2311@gmail.com>
2019-11-23 18:22:55 -0500

    Added deployment to Surge and some routing module clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,13 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent }, 
-      {path:'products/:productId', component: ProductDetailsComponent}  
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
