285debc1888de46bc1bf656eb0cb58bf05721d26
Kamal Joshi <kamal2311@gmail.com>
2019-11-27 12:08:01 -0500

    Added Jenkins pipeline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,12 @@ import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
 
 
-const routes: Routes = [{ path: '', component: ProductListComponent }, 
-{path:'products/:productId', component: ProductDetailsComponent}, 
-{path: 'cart', component: CartComponent},{path:'shipping', component:ShippingComponent} ];
+const routes: Routes = [
+  { path:'', component: ProductListComponent }, 
+  { path:'products/:productId', component: ProductDetailsComponent}, 
+  { path:'cart', component: CartComponent},
+  { path:'shipping', component:ShippingComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
